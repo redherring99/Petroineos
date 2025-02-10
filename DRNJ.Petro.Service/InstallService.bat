64049595fe630ceb06d8941b274e3c5dbba1ad62
David Jeffery <dj@drnj.co.uk>
2025-02-10 12:12:02 +0000

    fix to commentary

--- DRNJ.Petro.Service/InstallService.bat
+++ DRNJ.Petro.Service/InstallService.bat
@@ -1,1 +1,2 @@
+REM Use yor own path here
 sc create "ADJAggregatorService" binPath=C:\_data\data\SVN\Projects\Interview\Petro\GitHub-Master\Petroineos\DRNJ.Petro.Service\bin\Debug\net6.0\DRNJ.Petro.Service.exe
